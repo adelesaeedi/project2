6bfbbef421e22b5f16fb8ab2e467976ce5bae25c
adelesaeedi <124927997+adelesaeedi@users.noreply.github.com>
2023-08-31 23:34:11 +0330

    2 commit

---
+++ calculat csharp/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using class_calculator;
+
+
+namespace calculat_csharp
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Operation1 obj = new Operation1();
+
+
+            int n, m;
+
+            Console.WriteLine("\t please enter first number ......!");
+            int.TryParse(Console.ReadLine(), out n);
+            Console.WriteLine("\t please enter second number......!");
+            int.TryParse(Console.ReadLine(), out m);
+
+
+
+
+
+            Console.WriteLine("\t Select the operation");
+            Console.WriteLine("\t + - add");
+            Console.WriteLine("\t - - Minus");
+            Console.WriteLine("\t * - Multiply");
+            Console.WriteLine("\t /- Divide");
+            Console.WriteLine("\t Your Operation?");
+
+            string operand = Console.ReadLine();
+            while (true)
+            {
+
+
+                switch (operand)
+                {
+
+                    case "+" or "sum":
+
+                        Console.WriteLine($" {n}+{m} = " + obj.Sum(n, m));
+                        break;
+                    case "-" or "minuse":
+                        Console.WriteLine($" {n}-{m} = " + obj.Minus(n, m));
+                        break;
+                    case "*" or "multiple":
+                        Console.WriteLine($" {n}*{m} = " + obj.Multiple(n, m));
+                        break;
+                    case "/" or "divide":
+                        Console.WriteLine($" {n}/{m} = " + obj.Divide(n, m));
+                        break;
+
+                    default:
+                        Console.WriteLine("\t the operand is wrong...");
+                        break;
+
+
+                }
+                Console.ReadKey();
+            }
+        }
+    }
+}
---
+++ class calculator/Operation1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace class_calculator
+{
+    public class Operation1
+    {
+        
+        public int Sum(int n, int m)
+        {
+
+            return n + m;
+
+        }
+        public int Minus(int n, int m)
+        {
+            return n - m;
+
+
+        }
+
+        public double Multiple(int n, int m)
+        {
+            return n * m;
+        }
+        public double Divide(int n, int m)
+        {
+
+            try
+            {
+
+                return n / m;
+            }
+            catch (DivideByZeroException)
+            {
+                Console.WriteLine("Divide number by zero ");
+
+                return 0;
+            }
+        }
+    }
+}
